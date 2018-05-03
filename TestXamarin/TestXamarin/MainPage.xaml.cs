b9eb2c1a45ebb172b08e939dc73e0ba19926271c
Abhipsa Misra <abmisr@microsoft.com>
2018-05-02 17:28:10 -0700

    initial uwp state

---
+++ TestXamarin/TestXamarin/MainPage.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Microsoft.Azure.Devices.Client;
+
+namespace TestXamarin
+{
+	public partial class MainPage : ContentPage
+	{
+        // String containing Hostname, Device Id & Device Key in one of the following formats:
+        //  "HostName=<iothub_host_name>;DeviceId=<device_id>;SharedAccessKey=<device_key>"
+        //  "HostName=<iothub_host_name>;CredentialType=SharedAccessSignature;DeviceId=<device_id>;SharedAccessSignature=SharedAccessSignature sr=<iot_host>/devices/<device_id>&sig=<token>&se=<expiry_time>";
+        // Either set the IOTHUB_DEVICE_CONN_STRING environment variable or within launchSettings.json:
+        private static string DeviceConnectionString = Environment.GetEnvironmentVariable("IOTHUB_DEVICE_CONN_STRING");
+
+        private static int MESSAGE_COUNT = 5;
+        private const int TEMPERATURE_THRESHOLD = 30;
+        private static String deviceId = "MyCSharpXamarinDevice";
+        private static float temperature;
+        private static float humidity;
+        private static Random rnd = new Random();
+
+        public MainPage()
+		{
+			InitializeComponent();
+        }
+
+        async void SendEvent(object sender, EventArgs e)
+        {
+            // TODO: deviceClient initialization needs to be modified as per coding standards
+            DeviceClient deviceClient = DeviceClient.CreateFromConnectionString(DeviceConnectionString, TransportType.Http1);
+            string dataBuffer;
+
+            Console.WriteLine("Device sending {0} messages to IoTHub...\n", MESSAGE_COUNT);
+
+            for (int count = 0; count < MESSAGE_COUNT; count++)
+            {
+                temperature = rnd.Next(20, 35);
+                humidity = rnd.Next(60, 80);
+                dataBuffer = string.Format("{{\"deviceId\":\"{0}\",\"messageId\":{1},\"temperature\":{2},\"humidity\":{3}}}", deviceId, count, temperature, humidity);
+                Message eventMessage = new Message(Encoding.UTF8.GetBytes(dataBuffer));
+                eventMessage.Properties.Add("temperatureAlert", (temperature > TEMPERATURE_THRESHOLD) ? "true" : "false");
+                Console.WriteLine("\t{0}> Sending message: {1}, Data: [{2}]", DateTime.Now.ToLocalTime(), count, dataBuffer);
+
+                await deviceClient.SendEventAsync(eventMessage).ConfigureAwait(false);
+            }
+        }
+
+        async void ReceiveCommands(object sender, EventArgs e)
+        {
+            // TODO: deviceClient initialization needs to be modified as per coding standards
+            DeviceClient deviceClient = DeviceClient.CreateFromConnectionString(DeviceConnectionString, TransportType.Http1);
+            Console.WriteLine("\nDevice waiting for commands from IoTHub...\n");
+            Message receivedMessage;
+            string messageData;
+
+            while (true)
+            {
+                receivedMessage = await deviceClient.ReceiveAsync().ConfigureAwait(false);
+
+                if (receivedMessage != null)
+                {
+                    messageData = Encoding.ASCII.GetString(receivedMessage.GetBytes());
+                    Console.WriteLine("\t{0}> Received message: {1}", DateTime.Now.ToLocalTime(), messageData);
+
+                    int propCount = 0;
+                    foreach (var prop in receivedMessage.Properties)
+                    {
+                        Console.WriteLine("\t\tProperty[{0}> Key={1} : Value={2}", propCount++, prop.Key, prop.Value);
+                    }
+
+                    await deviceClient.CompleteAsync(receivedMessage).ConfigureAwait(false);
+                }
+
+                //  Note: In this sample, the polling interval is set to 
+                //  10 seconds to enable you to see messages as they are sent.
+                //  To enable an IoT solution to scale, you should extend this //  interval. For example, to scale to 1 million devices, set 
+                //  the polling interval to 25 minutes.
+                //  For further information, see
+                //  https://azure.microsoft.com/documentation/articles/iot-hub-devguide/#messaging
+                Thread.Sleep(10000);
+            }
+        }
+    }
+}
